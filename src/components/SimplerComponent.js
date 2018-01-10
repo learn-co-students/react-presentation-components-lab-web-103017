a5839b9d73c96b05cf8503e729bc378f1168438f
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2018-01-10 13:09:41 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,7 +1,7 @@
 // Code SimplerComponentHere Here
 import React from 'react';
 
-const mood = 'happy';
+// const mood = 'happy';
 const SimplerComponent = (props) =>
  <div
  onClick={props.handleClick}
