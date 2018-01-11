62fb5bea0a2155e5efe84dc8480302fcf8765302
jjwumaster <jjwumaster@gmail.com>
2018-01-11 16:56:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent
+      handleClick={e => {
+        e.target.style.color = "green";
+      }}
+    />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
